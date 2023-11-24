5c9a8ffabd32f30a27b75864e5e3cc3c5291621e
kimjeongsoo <dh65a02@naver.com>
2023-11-25 00:10:37 +0900

    docs: req.txt & gitignore

--- OneDrive/바탕 화면/djangotest/modelproject/accounts/views.py
+++
@@ -1,8 +0,0 @@
-from .models import CustomUser
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-from django.contrib import auth
-from django.contrib.auth.hashers import make_password
-
-# Create your views here.
--- OneDrive/바탕 화면/djangotest/modelproject/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-app_name = 'blog'
-
-urlpatterns = [
-    path('', BlogList.as_view()),
-    path('<int:pk>/', BlogDetail.as_view()),
-]
