887e96c16e7e1c6675738c7c8ce93676a0923151
ShinSub98 <wjdtjq26@naver.com>
2024-01-03 15:46:51 +0900

    feat: add image resizing logic

--- review/models.py
+++ review/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from accounts.models import User
 from restaurant.models import Restaurant
 import uuid
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 def review_image_path(instance, filename):
     ext = filename.split('.')[-1]
@@ -30,7 +33,45 @@ class ReviewImage(models.Model):
     리뷰 사진 model
     """
     review = models.ForeignKey(Review, on_delete = models.CASCADE)
-    review_image = models.ImageField(upload_to = review_image_path, blank = True)
+    review_image = models.ImageField(upload_to = review_image_path, null = True, blank = True) # 축소된 이미지
+    original_image = models.ImageField(upload_to = review_image_path, blank = True) # 원본 이미지
+
+    def save(self, *args, **kwargs):
+        if self.original_image:
+            image = Image.open(self.original_image)
+
+            # 축소할 기준 크기 설정
+            base_size = (100, 100)  # 원하는 기준 크기로 설정
+
+            # 이미지의 가로, 세로 크기를 가져옴
+            width, height = image.size
+
+            # 이미지의 가로 세로 비율 계산
+            ratio = min(base_size[0] / width, base_size[1] / height)
+
+            # 새로운 크기 계산
+            new_size = (int(width * ratio), int(height * ratio))
+
+            # 이미지를 새로운 크기로 조절하여 저장
+            image.thumbnail(new_size)
+
+            # 축소된 이미지 경로 설정
+            if not self.review_image:
+                self.review_image.name = review_image_path(self, self.original_image.name)
+
+            # Save the review_image field
+            image_thumb_io = BytesIO()
+            cropped_image = image.convert('RGB')
+            cropped_image.save(image_thumb_io, format='JPEG')
+            image_thumb = SimpleUploadedFile(
+                name=f'{self.original_image.name.split(".")[0]}_thumb.jpg',
+                content=image_thumb_io.getvalue(),
+                content_type='image/jpeg'
+            )
+            self.review_image.save(image_thumb.name, image_thumb, save=False)
+
+        # Call the superclass save method
+        super().save(*args, **kwargs)
 
 
 class TranslatedReview(models.Model):
--- review/serializers.py
+++ review/serializers.py
@@ -2,12 +2,57 @@ from rest_framework import serializers
 from .models import *
 
 class ReviewImageSerializer(serializers.ModelSerializer):
+    original_image = serializers.SerializerMethodField()
     """
-    리뷰 사진 시리얼라이저
+    리뷰 사진 업로드 및 원본 시리얼라이저
     """
+    
     class Meta:
         model = ReviewImage
-        fields = '__all__'
+        fields = ['id', 'original_image']
+
+    def get_original_image(self, obj):
+        if obj.original_image:
+            request = self.context.get('request')
+            return request.build_absolute_uri(obj.original_image.url)
+        return None
+
+class ThumbImageSerializer(serializers.ModelSerializer):
+    """
+    축소된 이미지 시리얼라이저
+    """
+    review_image = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ReviewImage
+        fields = ['id', 'review_image']
+
+    def get_review_image(self, obj):
+        if obj.review_image:
+            request = self.context.get('request')
+            return request.build_absolute_uri(obj.review_image.url)
+        return None
+
+
+class ImageSetSerializer(serializers.ModelSerializer):
+    review_image = serializers.SerializerMethodField()
+    original_image = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ReviewImage
+        fields = ['id', 'review_image', 'original_image']
+
+    def get_original_image(self, obj):
+        if obj.original_image:
+            request = self.context.get('request')
+            return request.build_absolute_uri(obj.original_image.url)
+        return None
+    
+    def get_review_image(self, obj):
+        if obj.review_image:
+            request = self.context.get('request')
+            return request.build_absolute_uri(obj.review_image.url)
+        return None
 
 
 class CreateReviewSerializer(serializers.ModelSerializer):
@@ -16,7 +61,7 @@ class CreateReviewSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = Review
-        exclude = ['user', 'restaurant', 'src_lang']
+        exclude = ['user', 'restaurant', 'src_lang', 'score']
 
 
 class ReviewInfoSerializer(serializers.ModelSerializer):
@@ -24,6 +69,11 @@ class ReviewInfoSerializer(serializers.ModelSerializer):
     리뷰 정보 시리얼라이저
     """
     image = ReviewImageSerializer(many = True)
+    def get_image(self, obj):
+        if obj.image:
+            request = self.context.get('request')
+            return request.build_absolute_uri(obj.image.url)
+        return None
     class Meta:
         model = Review
         fields = '__all__'
