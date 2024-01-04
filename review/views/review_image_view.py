458e84e05a35414d39edd31cad782ddb1b1466ff
ShinSub98 <wjdtjq26@naver.com>
2024-01-04 13:21:40 +0900

    feat: add original image view

--- review/views/review_image_view.py
+++ review/views/review_image_view.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from ..serializers import *
@@ -13,7 +14,7 @@ class ReviewImageView(CreateAPIView):
     """
     이미지 업로드 view
     """
-    serializer_class = ReviewImageSerializer
+    serializer_class = OriginalImageSerializer
 
     def create(self, request, review_id):
         review = get_object_or_404(Review, pk = review_id)
@@ -31,3 +32,28 @@ class ReviewImageView(CreateAPIView):
             return Response(res)
         else:
             return Response(serializer.errors)
+        
+
+class OriginalImageView(APIView):
+    """
+    이미지 원본 불러오기 view
+    """
+    def get(self, request, image_id):
+        image = get_object_or_404(ReviewImage, pk = image_id)
+        serializer = OriginalImageSerializer(image, context={'request': request})
+        res = {
+            "msg" : "이미지 원본 불러오기 성공",
+            "data" : {
+                "original_image" : serializer.data
+            }
+        }
+
+        return Response(res)
+    
+
+class AllImageView(ListAPIView):
+    """
+    (개발용)DB상 모든 이미지 데이터 확인 view
+    """
+    serializer_class = ImageSetSerializer
+    queryset = ReviewImage.objects.all()
