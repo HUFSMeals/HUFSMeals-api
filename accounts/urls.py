be21aff1b8fc539fcf8810b7261a9678f249eae0
ShinSub98 <wjdtjq26@naver.com>
2024-01-04 14:53:54 +0900

    feat: add user info view

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/<str:code>/', GoogleLogin.as_view()),
     path('nickname/', SetNickname.as_view()),
+    path('info/<int:user_id>/', UserInfoView.as_view()),
 
     # 개발자용
     path('google/', GoogleLoginApi.as_view()),
