233ec4667f0ebedea28738c8827282fd516c423b
ShinSub98 <wjdtjq26@naver.com>
2024-01-04 12:57:47 +0900

    feat: add image upload serializer

--- review/serializers.py
+++ review/serializers.py
@@ -4,18 +4,29 @@ from .models import *
 class ReviewImageSerializer(serializers.ModelSerializer):
     original_image = serializers.SerializerMethodField()
     """
-    리뷰 사진 업로드 및 원본 시리얼라이저
+    리뷰 사진 원본 시리얼라이저
     """
     
     class Meta:
         model = ReviewImage
-        fields = ['id', 'original_image']
+        fields = ['original_image']
 
     def get_original_image(self, obj):
         if obj.original_image:
             request = self.context.get('request')
             return request.build_absolute_uri(obj.original_image.url)
         return None
+    
+
+class ImageUploadSerializer(serializers.ModelSerializer):
+    """
+    리뷰 사진 업로드 시리얼라이저
+    """
+    
+    class Meta:
+        model = ReviewImage
+        fields = ['original_image']
+
 
 class ThumbImageSerializer(serializers.ModelSerializer):
     """
