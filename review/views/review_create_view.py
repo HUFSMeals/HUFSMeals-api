53438ddbc69df7a940c86bdc34e2cdd19adfc29c
ShinSub98 <wjdtjq26@naver.com>
2024-01-05 20:30:42 +0900

    refactor: add score calculation logic in ReviewCreateView

--- review/views/review_create_view.py
+++ review/views/review_create_view.py
@@ -19,7 +19,13 @@ class ReviewCreateView(CreateAPIView):
         restaurant = get_object_or_404(Restaurant, pk = restaurant_id)
         user = request.user
         src_lang = langcode_dev(request.data['body'])
-        score = 5
+        score = request.data['score']
+
+        restaurant.review_cnt += 1
+        restaurant.score_accum += int(score)
+        restaurant.score_avg = (restaurant.score_accum/restaurant.review_cnt)
+        restaurant.save()
+        
 
         data = {
             'title' : request.data['title'],
