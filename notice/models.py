5713a075f7889d4136def8adc09244aa3aba7f92
ShinSub98 <wjdtjq26@naver.com>
2024-01-15 12:21:01 +0900

    feat: add Notice model

--- notice/models.py
+++ notice/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Notice(models.Model):
+    title =  models.CharField(max_length = 100)
+    body = models.TextField(null = False)
+    created_at = models.DateField(auto_now_add = True)
+
+    def __str__(self):
+        return self.title
