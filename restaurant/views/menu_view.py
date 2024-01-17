8f3093285c95c67a4fb6cabfba8705a294b60f63
ShinSub98 <wjdtjq26@naver.com>
2024-01-17 15:33:11 +0900

    fix: add is_valid()

--- restaurant/views/menu_view.py
+++ restaurant/views/menu_view.py
@@ -24,11 +24,18 @@ class CreateMenuView(CreateAPIView):
     def post(self, request, restaurant_id):
         restaurant = get_object_or_404(Restaurant, pk = restaurant_id)
         serializer = CreateMenuSerializer(data = request.data)
-        menu = serializer.save(restaurant = restaurant)
-        res = {
-            "msg" : "메뉴 등록 완료",
-            "data" : {
-                MenuInfoSerializer(menu).data
+        if serializer.is_valid():
+            menu = serializer.save(restaurant = restaurant)
+            res = {
+                "msg" : "메뉴 등록 완료",
+                "data" : {
+                    MenuInfoSerializer(menu).data
+                }
             }
-        }
-        return Response(res, status = status.HTTP_200_OK)
+            return Response(res, status = status.HTTP_200_OK)
+        else:
+            res = {
+                "msg" : "유효하지 않은 데이터",
+            }
+            return Response(res, status = status.HTTP_400_BAD_REQUEST)
+        
