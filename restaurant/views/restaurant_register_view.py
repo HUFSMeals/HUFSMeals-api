b9da56eda4d56b45fc688971be10bee7231a7c2e
ShinSub98 <wjdtjq26@naver.com>
2024-01-18 13:56:02 +0900

    feat: add restaurant update view

--- restaurant/views/restaurant_register_view.py
+++ restaurant/views/restaurant_register_view.py
@@ -1,10 +1,11 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from ..serializers import *
 from ..models import *
 from django.db.models import Q
 import json
+from django.shortcuts import get_object_or_404
 
 class RestaurantRegisterView(CreateAPIView):
     """
@@ -12,3 +13,16 @@ class RestaurantRegisterView(CreateAPIView):
     """
     serializer_class = CreateRestaurantSerializer
     queryset = Restaurant.objects.all()
+
+
+class RestaurantUpdateView(UpdateAPIView):
+    """
+    식당 정보 수정 view
+    """
+    serializer_class = CreateRestaurantSerializer
+    queryset = Restaurant.objects.all()
+    
+    def get(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
