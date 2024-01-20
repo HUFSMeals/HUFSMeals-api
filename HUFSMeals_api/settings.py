e205ad39423d4ce047f6b278ba7169b8438a3c71
ShinSub98 <wjdtjq26@naver.com>
2024-01-20 13:12:07 +0900

    chore: connect mysql container

--- HUFSMeals_api/settings.py
+++ HUFSMeals_api/settings.py
@@ -117,7 +117,7 @@ DATABASES = {
         'NAME': 'hufsmeals',
         'USER': 'root',
         'PASSWORD': config('db_password'),
-        'HOST': 'recursing_carson',
+        'HOST': '172.17.0.2',
         'PORT': '3306',
     }
 }
