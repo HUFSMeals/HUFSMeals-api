eef7823d5b874aa5cd3031db0912619c269916d6
ShinSub98 <wjdtjq26@naver.com>
2024-01-21 16:10:31 +0900

    chore: change databases user from worker to root

--- HUFSMeals_api/settings.py
+++ HUFSMeals_api/settings.py
@@ -115,7 +115,7 @@ DATABASES = {
     'default' : {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'hufsmeals',
-        'USER': 'worker',
+        'USER': 'root',
         'PASSWORD': config('db_password'),
         'HOST': 'mysql_db',
         'PORT': '3305',
