31211da11f649105a3238742d0d0138720fbddf7
ShinSub98 <wjdtjq26@naver.com>
2024-02-12 10:28:01 +0900

    chore: set lifetime of token

--- HUFSMeals_api/settings.py
+++ HUFSMeals_api/settings.py
@@ -217,8 +217,8 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_EMAIL_REQUIRED = False
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=15),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=3),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
